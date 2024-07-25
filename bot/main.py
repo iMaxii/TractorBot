72e746d39e19fe184fe299d5319e28dffea571d7
iMaxii <110350308+iMaxii@users.noreply.github.com>
2024-07-25 19:35:46 -0400

    Automatically load all existing cogs

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,7 @@ import discord
 from cogs.core.autoroles import Autoroles, Verification
 from discord.ext import commands
 
-data_path = f"{os.getcwd()}/data/data.json"
+data_path = f"{os.getcwd()}/bot/data/data.json"
 with open(data_path, encoding="utf-8") as file:
     data = json.load(file)
 
@@ -55,25 +55,20 @@ class Client(commands.Bot):
             help_command=None,
         )
 
-        self.dirs = [
-            f"{os.getcwd()}/cogs/cmds",
-            f"{os.getcwd()}/cogs/core",
-        ]
         self.cogs_loaded = []
 
     async def setup_hook(self) -> None:
-        for dir in self.dirs:
-            for file in os.listdir(dir):
-                if file.endswith(".py"):
-                    cog = file[:-3]
-                    if dir.endswith("cmds"):
-                        await self.load_extension(f"cogs.cmds.{cog}")
-                        self.cogs_loaded.append(f"cmds.{cog}")
-                    elif dir.endswith("core"):
-                        await self.load_extension(f"cogs.core.{cog}")
-                        self.cogs_loaded.append(f"core.{cog}")
-                    else:
-                        print(f"Ha ocurrido un error cargando el cog {cog}.")
+        for root, d, files in os.walk(f"{os.getcwd()}/bot/cogs", topdown=True):
+            for f in files:
+                if f.endswith(".py"):
+                    dir = root[len(f"{os.getcwd()}/bot/cogs/") :]
+                    dir = dir.replace(os.sep, ".")
+                    cog = f[:-3]
+                    await self.load_extension(
+                        f"cogs.{dir}.{cog}" if dir else f"cogs.{cog}"
+                    )
+                    self.cogs_loaded.append(f"{cog}")
+
         client.add_view(Verification())
         client.add_view(Autoroles())
 
